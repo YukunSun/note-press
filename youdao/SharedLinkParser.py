ca932b722bb545c9365f90019a49bbdf0deb771d
sunyk <sunyukun@shopin.cn>
2019-12-01 23:12:17 +0800

    parse shared info

--- youdao/SharedLinkParser.py
+++ youdao/SharedLinkParser.py
@@ -5,6 +5,11 @@ import requests
 from urllib.parse import urlparse
 import yaml
 
+# 笔记概要信息：title、ctime etc.
+URL_SHARE_INFO = 'https://note.youdao.com/yws/public/note/{}'
+# 笔记的具体
+URL_DETAIL = "https://note.youdao.com/yws/api/personal/file/{}?method=download&read=true&shareKey={}&cstk=HncM6MEm"
+
 
 # 根据 url 链接 获取id
 def parse_note_id_by_shared_url(url):
@@ -24,7 +29,18 @@ for note in notes:
     url = note['url']
     if url is None:
         continue
-    # 获取 id
-    note_id = parse_note_id_by_shared_url(url)
-    print(note_id)
-
+    # 截取 shareKey
+    share_key = parse_note_id_by_shared_url(url)
+    if share_key is None:
+        continue
+    # 获取 title 等信息
+    url_share = URL_SHARE_INFO.format(share_key)
+    shared_info = requests.get(url_share).json()
+    p = shared_info.get('p')[1:]
+    ctime = shared_info.get('ct')
+    utime = shared_info.get('mt')
+    title = shared_info.get('tl')
+    url_detail = URL_DETAIL.format(p, share_key)
+    # todo
+    note_detail = requests.post(url_detail).json()
+    print(note_detail)
